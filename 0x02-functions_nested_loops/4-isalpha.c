c9c2eabe8544ebb962a42239e6fb16dade22285f
hammed71 <wasiuhammed71@gmail.com>
2022-12-13 12:01:07 -0800

    printing out alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - this is a program to prints out alphabets
+ * @c: characters
+ * Return: 1 if c is aletter, lowercase or uppercase, or 0 otherwise
+ */
+
+int int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
